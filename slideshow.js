8fa7f75830ea07746e8ec02cc5cff7d8a3a9e02d
kimluu1701 <kimmie@Kims-MacBook-Pro.local>
2017-05-04 15:14:15 +1000

    Update about us

--- slideshow.js
+++
@@ -1,7 +0,0 @@
-var slideimages = new Array () //create new array to preload images
-slideimages[0] = new Image ()
-slideimages[0].scr = "images/slide1.jpg"
-slideimages[1] = new Image ()
-slideimages[1].scr = "images/slide2.jpg"
-slideimages[2] = new Image ()
-slideimages[2].scr = "images/slide3.jpg"
